34d8e27921943272e447722cbc04b244a4e1d995
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-03 22:55:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,60 +18,32 @@ export default function Home() {
       <p className="z-10 whitespace-pre-wrap text-center text-xl font-extrabold tracking-tighter text-black dark:text-white">
        I DONT UNDERSTAND WHATEVER THE LANG YOU USE, KINDLY REPLY IN HINDI/ENGLISH ONLY PLEASE?
         <br />
-        <span className=" text-base font-medium">
-          {' '}
-           {'"weak" and why should I even text you back?'} 
-                    <br />
-
-          For another direct complain to my parents? <br /> <br />
-          <br />
-          Still, {'"weak"? '} <br />
-          You abusing my sister, mocking at my works, blocking me dozens of time
-          wasnt weakening? <br />
-          Its not the text based that weakens but rather the opposite
-          un-trustworthy person <br /> <br /> <br />
-          Just a month {"you're"} already following itz_s_732? If you think I
-          dont have any stalkers, well there are many but for confirmation check
-          out @digital_nitya27/_pallavi_deyy/_vanshikaaaa26_ and name initial as the Name
-          {/*  <br /> Dont believe me?
-          <br />
-          Let me just follow one  */} <br /> <br /> <br />
-          The only reason I couldnt make a mom-promise that {"you'd"} be my
-          lifetime partner because I dont really trust you after all the lies
-          and abusing {"you've"} done <br /> <br /> <br />
-          Look I wont really just simp over just because how beautiful you look
-          and cry over and over again, as far I only loved your beautiful soul with a heartwarming dream<br />
-          Also you have a chappri mindset you literally follow a boy with{' '}
-          {'"itz"'} in the username and say to me text based are weak, then want
-          do you want? <br /> Should I ride with you on a KTM? <br />
-          And make lifetime promises and get caught with a new person the next
-          day, instead of working and completing my dreams? <br />
-          You just made me getting hurt by your actions a sort of daily habit{' '}
-          <br />
-          Dont trust me just leave me alone <br /> <br />
-          <br />
-          Following a boy i_s_732? <br />
-          When you said your dad dont allow you to talk to us how much time it
-          took me to say just block me then? <br />I remembered I forgot to ask
-          for help because{' '}
-          {
-            '"a g" was physically harassing me and it was literalling screwing up my mental health and what you did in return, just blocked me?'
-          }{' '}
-          <br />
-          And you think I should trust you? <br /> <br /> <br />
-          Could you make a promise that you wont be talking to a boy ever again
-          till we meet again (valid only in emergency conditions)? <br />I could
-          do the same promise easily without even a second thought <br /> <br />{' '}
-          <br />
-          Also if you think {"I'm"} active talking to other gs on discord or
-          other russian platforms then <br />I am aleady having the last year of
-          my chance of qualifing jee adv, {"I'm"} busy <br />
-          Want a challenge? <br />
-          Qualify NEET and become a doctor or I will never be your partner,
-          enough of kindly asking to study, now its a challenge <br />
-          Perfect solution for you and me to stop wasting time <br /> <br />{' '}
-          <br />
-        </span>
+        Bruh SSB why did you also had to be like everyone else betrayer <br/>
+        Jante ho mere parents aur mera zindagi, movie se kamm nahi bloge<br/>
+        mere dadaji apna time pe cycle mein nitro fit kardiye the, radio khud se hi bana diye the (from scratch), apna cycle ko motorcycle bana diye the <br/>
+        uss samay yeh sab bada chiz tha, mera dadaji ka 22 acre land property + Verma Radio Ghar (Tetulmari + Katras Business Shop) establish kardiye, apna time ka best-in-class, overall best electronics shop<br/>
+        Bass unka ek problem tha over-trusting (joh ki mere papa aur mera bhi hai), apne bhaiyo aur mere bada-papa log pe <br/>
+        apna 11 acre land uss zamana mein apne bhaiyo ko free mein dediye bharosa karke <br/>
+        aur badle mein, mere papa jab 9 yrs k the tb hi mere dadaji ka death hogya mysteriously <br/>
+        mere papa ko nahi pata chala death ka reason naa hi meri dadi ko <br/>
+        mere papa ko 18 yrs, mature hone takk ekdm king wala treatment diya gya <br/>
+        aur uske baad sab property pe sign karwa ke le liya gya <br/>
+        mere papa bharosa karke sign karr bhi diye <br/>
+        mere papa hi se sab ghr ka kaam karwaya jaata tha kapda dhona, gadi saaf krwana <br/>
+        mere papa ka Jharkhand mein rank 2 aagya tha uss samay ka regional programming contest mein, infosys se invitation mila, mere bade-papa log jaan buch ke papa ko nahi jaane diya papa ko voh letter diya hi nahi gya, baad mein officials aaye batane toh unko bol diya gya ki koi Dipen Kumar Verma (mere papa) yaha hai hi nahi
+       roz milk feka jaata tha parr papa ko nahi milta tha, ghar mein 10-20 kg meat banta tha parr papa aur dadi ko sirf 2 piece milta tha, dadi apna bhi papa ke liye baccha deti thi <br/>
+        <br/>
+        aur uske baad matt jaano, short mein bata rha hu <br/>
+        mummy aayi, mummy ka saara jewellery dheere dheere le liya gya, yaha takk ki roz spoons chori ho jaata tha<br/>
+        papa ko promise mila ki 1-2 yrs mein alag ghar banake dedenge, parr kuch hua nahi <br/>
+        mere papa ka zindagi barbaad kardiya voh log aur abhi besharam log bolta hai voh log ka property hai, dehati log circuit ka matlab nahi jaanta hoga thik se<br/>
+        <br/>
+        mere saath bhi abhi takk toh sab jagah se betrayal hi mila hai, tumse, saara online dost se<br/>
+        isiliye mummy merepe itna restrictions lagayi hai, samjhe?
+        <br/>
+        humlog ka weakness hi trust hai, aur aise hi log mera bhi zindagi mein aate hai <br/>
+        sacch bolu toh suicidal feelings aata hai kabhi kabhi, ek din kabhi karbhi lunga, abb sahem bhi nhi hota hai, vhir apne papa la heartbroken smile aur mummy yaad aajaati hai <br/>
+        aur sacch bolu toh jab se aap mera zindagi mein aaye ho mera academics mein khatarnak downfall aaya hai no lies
       </p>
     </main>
   );
