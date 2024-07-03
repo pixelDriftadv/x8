33e3f92c3b26c7e7e60b64fce8f78a9a8f9d4d5b
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-04 04:39:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         )}
       />
       <p className="z-10 whitespace-pre-wrap text-center text-sm font-extrabold tracking-tighter text-black dark:text-white">
-       If I still couldnt make you unblock and make me a friend then goodbye, enjow your life with i_s_732, im leaving everything now, dont ever try reaching me ever again neither I would</p>
+       If I still couldnt make you unblock and make me a friend then goodbye, enjoy your life with i_s_732, im leaving everything now, dont ever try reaching me ever again neither I would</p>
     </main>
   );
 }
